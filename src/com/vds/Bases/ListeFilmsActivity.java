0cd633822afa46f919f92d7589469c8b58b961b2
Ulysse Gérard <thevoodoos@gmail.com>
2014-06-02 15:18:32 +0200

    Initial commit

---
+++ src/com/vds/Bases/ListeFilmsActivity.java
@@ -0,0 +1,19 @@
+package com.vds.Bases;
+
+/**
+ * Created by Ulysse on 02/06/2014.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListeFilmsActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listefilms);
+    }
+}
